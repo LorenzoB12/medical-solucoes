e02cbddae0d126a52bfeed2649c7fab519050a91
Carlos <carlosedu.born@gmail.com>
2023-06-14 20:24:04 -0300

    Modificado db

--- db.sql
+++ db.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS usuarios
     ativo boolean NOT NULL,
     CONSTRAINT usuarios_pkey PRIMARY KEY (id)
 )
+
+CREATE TABLE IF NOT EXISTS especialidades
+(
+    id integer NOT NULL DEFAULT nextval('especialidades_id_seq'::regclass),
+    descricao text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT especialidades_pkey PRIMARY KEY (id)
+)
+
+INSERT INTO usuarios (id, nome, email, login, senha, ativo) VALUES (1, 'admin', 'admin@medical.com.br', 'admin', '$2a$10$4r2otWRd.CwAGpGFenxxc.fpy5UMf1g/lYfbZQLEyhdDhG14qqDUy', true);
