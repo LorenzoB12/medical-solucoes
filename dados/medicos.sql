cd67fab1b88aeb7019dd57a412388bc97083a116
Carlos <carlosedu.born@gmail.com>
2023-06-18 10:10:58 -0300

    Adicionado pasta com dados ficticios

---
+++ dados/medicos.sql
@@ -0,0 +1,24 @@
+INSERT INTO doctors (nome,crm,ativo,especialidades_id)
+VALUES
+  ('Zena Solomon','XRM/RS 712514','false',2),
+  ('Dawn Rodgers','PRM/RS 353475','true',3),
+  ('TaShya Salazar','VRM/RS 274961','false',4),
+  ('Gary Mack','YRM/RS 526186','true',3),
+  ('James Pennington','GRM/RS 783133','true',4),
+  ('Kato Barnes','JRM/RS 973588','false',1),
+  ('Palmer Bray','VRM/RS 673638','true',3),
+  ('Fallon Bonner','HRM/RS 724387','false',1),
+  ('Mia Terrell','GRM/RS 725895','false',1),
+  ('Kiayada Mcclure','NRM/RS 146046','false',3);
+INSERT INTO doctors (nome,crm,ativo,especialidades_id)
+VALUES
+  ('Avye Turner','ZRM/RS 153531','false',3),
+  ('Neve Floyd','CRM/RS 937816','false',3),
+  ('Jerry Burris','XRM/RS 313342','false',4),
+  ('Ryder George','PRM/RS 883263','false',2),
+  ('Roanna Petersen','DRM/RS 793677','true',3),
+  ('Avye Moody','FRM/RS 085671','true',4),
+  ('Amos Crosby','XRM/RS 181532','false',3),
+  ('Rogan Bowen','LRM/RS 178380','false',2),
+  ('Rinah Lopez','JRM/RS 884283','true',4),
+  ('Hashim Chen','WRM/RS 124587','true',3);
---
+++ dados/pacientes.sql
@@ -0,0 +1,24 @@
+INSERT INTO pacientes (nome,data_nascimento,cpf,telefone,genero,ativo)
+VALUES
+  ('Zena Dunlap','1947-01-23','32658745228','51954595369','MASCULINO','false'),
+  ('Logan Gonzales','1961-03-18','32175262353','51906715768','MASCULINO','true'),
+  ('Elaine Mercer','1995-04-01','55028507416','51998147851','FEMININO','true'),
+  ('Gareth Shields','1978-02-08','66064828282','51951174356','FEMININO','false'),
+  ('Darius Dejesus','1985-03-20','76247984965','51994220475','MASCULINO','true'),
+  ('Audrey Sanders','1981-07-11','92213611236','51947558610','MASCULINO','false'),
+  ('Henry Goodwin','1958-01-17','14221288211','51937243517','FEMININO','false'),
+  ('Baker Manning','1977-09-17','12145175447','51915758671','FEMININO','false'),
+  ('Isaiah Frye','1983-05-12','61521621373','51967433371','MASCULINO','true'),
+  ('Jin Montgomery','1972-11-18','36528582867','51935287284','MASCULINO','true');
+INSERT INTO pacientes (nome,data_nascimento,cpf,telefone,genero,ativo)
+VALUES
+  ('Burton Ferguson','1985-07-04','63243425268','51943648351','FEMININO','false'),
+  ('Danielle Benjamin','1964-08-25','58532585668','51934276843','FEMININO','false'),
+  ('Genevieve England','1979-12-29','63662951138','51915663114','MASCULINO','false'),
+  ('Berk Chaney','1973-08-02','97261652555','51955746141','MASCULINO','true'),
+  ('Noelle Ryan','1967-02-25','92575396297','51925763429','FEMININO','true'),
+  ('Nell Melton','1948-06-24','82243244943','51935234318','FEMININO','true'),
+  ('Rosalyn Buckner','1977-04-24','24666286233','51928746870','MASCULINO','true'),
+  ('Harlan Blake','1989-11-03','75421453634','51937561943','MASCULINO','false'),
+  ('Hunter Andrews','1963-06-26','21011215472','51972796833','FEMININO','true'),
+  ('Gavin Gilliam','1951-08-25','93734722873','51961563666','FEMININO','false');
